c14ddca144680c16577e78e65e1ed6ae7999e461
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-12-12 01:03:32 +0300

    1st

---
+++ less2/extra1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+void Teorema()
+{
+    List<int> chisla = new List<int>();
+    int a = 86;
+    if (a % 2 != 0)
+    {
+       for (int i = 4;i < a;i++)
+       {
+            for (int j = 2; j < i;j ++)
+            {
+                if ((i % j != 0) && (i == j +1))
+                {
+                    chisla.Append(i);
+                    Console.WriteLine($"{i}");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+       }
+    }
+}
+
+Teorema();
