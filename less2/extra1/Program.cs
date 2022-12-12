2684c4dfc0b1613846a4c5aa139a1ef9e9c53534
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-12-12 17:48:22 +0300

    resheno

--- less2/extra1/Program.cs
+++ less2/extra1/Program.cs
@@ -1,26 +1,21 @@
-﻿
-void Teorema()
-{
-    List<int> chisla = new List<int>();
-    int a = 86;
-    if (a % 2 != 0)
-    {
-       for (int i = 4;i < a;i++)
-       {
-            for (int j = 2; j < i;j ++)
-            {
-                if ((i % j != 0) && (i == j +1))
-                {
-                    chisla.Append(i);
-                    Console.WriteLine($"{i}");
-                }
-                else
-                {
-                    continue;
-                }
-            }
-       }
-    }
-}
+﻿int[] mass = new int[1000];
 
-Teorema();
+int main(int n)
+{
+    int i, j;
+    for (i = 2; i <= n; i++) mass[i] = i;
+
+    for (j = 2; 2 * j <= n; j++)
+        for (i = j + j; i <= n; i += j) mass[i] = 0;
+
+    for (i = 2; 2 * i <= n; i++)
+        if ((mass[i] != 0) && (mass[n - i] != 0))
+        {
+            // вывод результата     
+            Console.WriteLine($"{mass[i]} and {mass[n-i]}");
+            break;
+        }
+    return 0;
+}
+int chislo = int.Parse(Console.ReadLine());
+main(chislo);
